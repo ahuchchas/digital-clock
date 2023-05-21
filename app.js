66f796520f92ad49371bfc672e71b6d52b745ca8
Abu Hurayra Uchchas <86414070+ahuchchas@users.noreply.github.com>
2023-05-21 22:56:08 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const progressBar = document.getElementById("progress");
 function updateTime() {
     let date = new Date();
     hourText.textContent = date.getHours() < 10 ? "0"+date.getHours() : date.getHours();;
-    minuteText.textContent = date.getMinutes() < 10 ? "0"+date.getMinutes() : date.getMinutes();;
+    minuteText.textContent = date.getMinutes() < 10 ? "0"+date.getMinutes() : date.getMinutes();
     secondText.textContent = date.getSeconds() < 10 ? "0"+date.getSeconds() : date.getSeconds();
     progressBar.style.width = (date.getSeconds()/60)*100 +"%";
 }
